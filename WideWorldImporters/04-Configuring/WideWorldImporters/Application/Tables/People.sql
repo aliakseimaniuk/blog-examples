6293614d31194ab22a0bb67a0253b59452865cfb
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-05-16 23:06:04 -0400

    Added configuration for Database project example.

---
+++ WideWorldImporters/04-Configuring/WideWorldImporters/Application/Tables/People.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE [Application].[People] (
+    [PersonID]     INT             NOT NULL,
+    [FullName]     NVARCHAR (50)   NOT NULL,
+    CONSTRAINT [PK_Application_People] PRIMARY KEY CLUSTERED ([PersonID] ASC)
+)
+
+GO
+EXECUTE sp_addextendedproperty
+    @name = N'Description',
+    @value = 'Full name for this person',
+    @level0type = N'SCHEMA',
+    @level0name = N'Application',
+    @level1type = N'TABLE',
+    @level1name = N'People',
+    @level2type = N'COLUMN',
+    @level2name = N'FullName';
+
+GO
+EXECUTE sp_addextendedproperty
+    @name = N'Description',
+    @value = 'Numeric ID used for reference to a person within the database',
+    @level0type = N'SCHEMA',
+    @level0name = N'Application',
+    @level1type = N'TABLE',
+    @level1name = N'People',
+    @level2type = N'COLUMN',
+    @level2name = N'PersonID';
+
+GO
+EXECUTE sp_addextendedproperty
+    @name = N'Description',
+    @value = N'People known to the application (staff, customer contacts, supplier contacts)',
+    @level0type = N'SCHEMA',
+    @level0name = N'Application',
+    @level1type = N'TABLE',
+    @level1name = N'People';
